11b95ee6b2be0937dd41dd567a5352afb839aadf
jltrench <jltrench@gmail.com>
2023-03-29 20:04:47 -0300

    Adicionando StatefulWidget e contador de level

--- lib/main.dart
+++ lib/main.dart
@@ -15,35 +15,155 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Stack(
-            alignment: Alignment.center,
-            children: [
-              Container(color: Colors.red, width: 100, height: 100,),
-              Container(color: Colors.blueAccent, width: 50, height: 50,)
-            ],
-          ),
-          Stack(
-            alignment: Alignment.center,
-            children: [
-              Container(color: Colors.blueAccent, width: 100, height: 100,),
-              Container(color: Colors.red, width: 50, height: 50,)
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(color: Colors.limeAccent, width: 50, height: 50,),
-              Container(color: Colors.orangeAccent, width: 50, height: 50,),
-              Container(color: Colors.indigo, width: 50, height: 50,)
-            ],
-          )
-        ],
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Container(),
+          title: const Text('Tarefas'),
+        ),
+        body: ListView(
+          children: const [
+            Task('Aprender Flutter'),
+            Task('Andar de Bike'),
+            Task('Estudar'),
+          ],
+        ),
+        floatingActionButton:
+            FloatingActionButton(onPressed: () => print('Teste')),
       ),
+    );
+  }
+}
+
+class Task extends StatefulWidget {
+  final String taskName;
+
+  const Task(this.taskName, {super.key});
+
+  @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int nivel = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: Stack(
+          children: [
+            Container(
+              color: Colors.blue,
+              height: 140,
+            ),
+            Column(
+              children: [
+                Container(
+                  color: Colors.white,
+                  height: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        color: Colors.black,
+                        width: 72,
+                        height: 100,
+                      ),
+                      Container(
+                        width: 200,
+                        child: Text(
+                          widget.taskName,
+                          style: const TextStyle(
+                              fontSize: 24, overflow: TextOverflow.ellipsis),
+                        ),
+                      ),
+                      Container(
+                        height: 52,
+                        width: 52,
+                        child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                if (nivel < 10) {
+                                  nivel++;
+                                }
+                              });
+                            },
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                const Icon(Icons.arrow_drop_up),
+                                Text('UP', style: TextStyle(fontSize: 12),)
+                              ],
+                            )),
+                      )
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        child: LinearProgressIndicator(
+                          color: Colors.white,
+                          value: nivel / 10,
+                        ),
+                        width: 200,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Text(
+                        'Nivel: $nivel',
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Teste extends StatefulWidget {
+  const Teste(this.altura, this.largura, this.cor, {super.key});
+
+  final double altura;
+  final double largura;
+  final Color cor;
+
+  @override
+  State<Teste> createState() => _TesteState();
+}
+
+class _TesteState extends State<Teste> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          color: widget.cor,
+          height: widget.altura,
+          width: widget.largura,
+        ),
+        Container(
+          color: widget.cor,
+          height: widget.altura,
+          width: widget.largura,
+        ),
+        Container(
+          color: widget.cor,
+          height: widget.altura,
+          width: widget.largura,
+        )
+      ],
     );
   }
 }
